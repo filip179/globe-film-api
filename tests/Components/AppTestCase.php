b18d9d4a09e29f07b225ec6f849401f1b4606d5d
Filip Zyminkowski <filip.zyminkowski@snow.dog>
2019-09-09 12:37:44 +0200

    DEV00002 - functional tests- fixed and reimplemented base App TestCase

--- tests/Components/AppTestCase.php
+++ tests/Components/AppTestCase.php
@@ -2,17 +2,34 @@
 
 namespace App\Tests\Components;
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class AppTestCase extends KernelTestCase
 {
-    private $manager;
+    protected $manager;
+    /** @var Client */
+    protected $http;
+
+    protected function get(string $uri)
+    {
+        $baseUrl = getenv('APP_HOST') ?? 'http://localhost';
+
+        return $this->http->request('GET', $baseUrl . $uri, ['headers' => [
+            'Accept' => 'application/json',
+            'Content-type' => 'application/json'
+        ]]);
+    }
 
     protected function setUp(): void
     {
         $kernel = self::bootKernel();
 
         $this->manager = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $this->http = new Client([
+            'exceptions' => false,
+        ]);
     }
 
     protected function tearDown(): void
