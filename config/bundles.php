bfe13ec2c13ca276a105d713a460d1d6f077976a
Filip Zyminkowski <filip.zyminkowski@snow.dog>
2019-09-12 09:58:30 +0200

    DEV00002 - functional tests - more configuration stuff for working tests, removed unused config files

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,6 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
